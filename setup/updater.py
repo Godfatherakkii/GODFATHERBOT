ca3ecde015d8dafd3b3c4a2f3bc99751145b842e
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-07-27 08:49:19 -0700

    Update updater.py

--- setup/updater.py
+++ setup/updater.py
@@ -43,7 +43,7 @@ async def update_requirements(main , test):
     try:
         for i in a:
             await runcmd(f"pip install {i}")
-            print(f">> Installed Requirement: {i}")
+            print(f">>💥Powers『ɢᴏᴅꜰᴀᴛʜᴇʀ』💥 : {i}")
     except Exception as e:
         print(f"Error installing requirments {str(e)}")
 
