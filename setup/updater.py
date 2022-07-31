1ee0591b3464da22a9fb31805e2b2bd1866cf60b
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-07-31 05:18:28 -0700

    Add files via upload

--- setup/updater.py
+++ setup/updater.py
@@ -43,7 +43,7 @@ async def update_requirements(main , test):
     try:
         for i in a:
             await runcmd(f"pip install {i}")
-            print(f">>💥Powers『ɢᴏᴅꜰᴀᴛʜᴇʀ』💥 : {i}")
+            print(f">> Installed Requirement: {i}")
     except Exception as e:
         print(f"Error installing requirments {str(e)}")
 
